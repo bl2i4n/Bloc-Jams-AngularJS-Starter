42122cf43c6fbafa654b4de30f8af54d0e744429
bl2i4n <bl2i4n@yahoo.com>
2017-09-11 20:17:13 -0700

    refactoring from other demo

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,33 @@
-angular.module('punchline', []);
+(function(){
+  function config($stateProvider, $locationProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        templateUrl: '/templates/landing.html'
+      })
+      .state('registration', {
+        url: '/registration',
+        templateUrl: '/templates/registration.html'
+      })
+      .state('user', {
+        url: '/user',
+        templateUrl: '/templates/user.html'
+      })
+      .state('filters', {
+        url: '/filters',
+        templateUrl: '/templates/filters.html'
+      });
+
+  }
+
+
+  angular
+    .module('punchline', ['ui.router'])
+    .config(config);
+})();
