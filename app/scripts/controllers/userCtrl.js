783561369c7e3bb31c362df87f4df1c2b14218da
bl2i4n <bl2i4n@yahoo.com>
2017-09-12 22:29:24 -0700

    followed angularfire tut on user auth

---
+++ app/scripts/controllers/userCtrl.js
@@ -0,0 +1,22 @@
+(function(){
+  function userCtrl($scope, userFactory){
+    $scope.createUser = function(){
+      $scope.message = null;
+      $scope.error = null;
+
+      // Create a new user
+      userFactory.$createUserWithEmailAndPassword($scope.email, $scope.password)
+        .then(function(firebaseUser){
+          $scope.message = "User created with uid: " + firebaseUser.uid;
+        }).catch(function(error){
+          $scope.error = error;
+        });
+    };
+
+  }
+
+  angular
+        .module('punchline')
+        .controller('userCtrl', ['$scope', 'userFactory', userCtrl]);
+
+})();
