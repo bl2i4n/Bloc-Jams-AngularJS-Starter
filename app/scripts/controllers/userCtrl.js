e35209847024e531d3d9fe23df1a8f9b9c16ec94
bl2i4n <bl2i4n@yahoo.com>
2017-09-14 10:35:52 -0700

    trying to retrieve user data with onAuthStateChanged

--- app/scripts/controllers/userCtrl.js
+++ app/scripts/controllers/userCtrl.js
@@ -4,13 +4,22 @@
       $scope.message = null;
       $scope.error = null;
 
+      //retrieve auth state
+      $scope.auth = userFactory;
+
       // Create a new user
       userFactory.$createUserWithEmailAndPassword($scope.email, $scope.password)
         .then(function(firebaseUser){
-          $scope.message = "User created with uid: " + firebaseUser.uid;
+          $scope.message = "User created with uid: " + firebaseUser.email;
+          $(".content").html(firebaseUser.email)
         }).catch(function(error){
           $scope.error = error;
         });
+
+        //TODO: Is this even getting called?
+      $scope.auth.$onAuthStateChanged(function(firebaseUser) {
+        $scope.firebaseUser = firebaseUser;
+      });
     };
 
   }
