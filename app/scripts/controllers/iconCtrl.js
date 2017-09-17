40856e4aef46e8cfbd20cf8a3381d22c946c3c19
bl2i4n <bl2i4n@yahoo.com>
2017-09-17 02:11:03 -0700

    could not push data to firebase, but going to try bloc-chat method

--- app/scripts/controllers/iconCtrl.js
+++ app/scripts/controllers/iconCtrl.js
@@ -2,9 +2,13 @@
   function iconCtrl($scope, $firebaseObject) {
     var ref = firebase.database().ref().child("data");
     //download the data into a local object
-    var syncIcon = $firebaseObject(ref);
-    //three-way data binding?
-    syncIcon.$bindTo($scope, "data");
+    // var syncIcon = $firebaseObject(ref);
+    // //three-way data binding?
+    // $scope.saveEntry = function(){
+    //   syncIcon.push($scope.icon);
+    // }
+    ref.push(data)
+
   }
 
   angular
