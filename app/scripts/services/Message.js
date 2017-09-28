3e045a5ddcb0e9ecb313cb587f756ff3b6c7b6a6
bl2i4n <bl2i4n@yahoo.com>
2017-09-27 22:13:57 -0700

    starting to create chat room vs. bot

---
+++ app/scripts/services/Message.js
@@ -0,0 +1,14 @@
+(function(){
+
+  function Message($firebaseArray){
+    var Message = {};
+    var ref = firebase.database().ref().child("messages");
+
+    return Message;
+  }
+
+  angular
+    .module('blocChat')
+    .factory('Message', ['$firebaseArray', Message]);
+
+})();
